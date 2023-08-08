55406590b767703b777c8539a590d773d9d44bc0
Jann Moreno <janncarlomoreno@gmail.com>
2023-08-08 01:08:33 -0500

    refactor (addWookieeFormat): Agrega el formato Wookiee a las url de peticiones en swapi endpoints

--- src/server/endpoints/swapiEndpoints.js
+++ src/server/endpoints/swapiEndpoints.js
@@ -1,16 +1,20 @@
 
-const _isWookieeFormat = (req) => {
-    if(req.query.format && req.query.format == 'wookiee'){
-        return true;
-    }
-    return false;
-}
+const _isWookieeFormat = req => req?.query?.format === 'wookiee'
+
+const BASE_URL = 'https://swapi.dev/api/';
+
+const _supportWookieFormat = (url, req) => url+(_isWookieeFormat(req)?'?format=wookiee':'')
+
+const HTTP_METHODS = {
+    GET: 'GET', POST: 'POST', PUT: 'PUT', DELETE: 'DELETE'
+};
+
 
 
 const applySwapiEndpoints = (server, app) => {
 
     server.get('/hfswapi/test', async (req, res) => {
-        const data = await app.swapiFunctions.genericRequest('https://swapi.dev/api/', 'GET', null, true);
+        const data = await app.swapiFunctions.genericRequest(_supportWookieFormat(BASE_URL, req), HTTP_METHODS.GET, null, true);
         res.send(data);
     });
 
