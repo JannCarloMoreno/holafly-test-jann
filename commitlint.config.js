8a79bd03d71f54b9e62fada1c405d2fb651ec2b7
Jann Moreno <janncarlomoreno@gmail.com>
2023-08-09 17:29:34 -0500

    chore(commitLint/husky): Add husky and commitlint package to lint commit messages

---
+++ commitlint.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [2, 'always', ['feat', 'fix', 'refactor', 'chore']],
+        'scope-case': [2, 'always', ['camel-case']],
+        'subject-case': [2, 'always', ['sentence-case']]
+    },
+}
