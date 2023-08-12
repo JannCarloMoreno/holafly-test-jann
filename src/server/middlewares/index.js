da664937b804aeaa75d3737f401ab029aabd2bc6
Jann Moreno <janncarlomoreno@gmail.com>
2023-08-12 01:47:43 -0500

    chore(test): Test the precommit hook

--- src/server/middlewares/index.js
+++ src/server/middlewares/index.js
@@ -2,6 +2,8 @@ const loggingMiddleware = require('./loggingMiddleware')
 const checkDbMiddleware = require('./checkDbMiddleware')
 
 const applyMiddlwares = (server, app) => {
+  const a = 2
+  console.log(a)
   server.use(loggingMiddleware(app.db))
   server.use(checkDbMiddleware(app.db))
   return server
