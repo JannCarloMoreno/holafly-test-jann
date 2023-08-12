77c59e8ce3a2851ebce221593ba84412227cc132
Jann Moreno <janncarlomoreno@gmail.com>
2023-08-12 01:49:06 -0500

    chore(commingBack): Restoring

--- src/server/middlewares/index.js
+++ src/server/middlewares/index.js
@@ -2,8 +2,6 @@ const loggingMiddleware = require('./loggingMiddleware')
 const checkDbMiddleware = require('./checkDbMiddleware')
 
 const applyMiddlwares = (server, app) => {
-  const a = 2
-  console.log(a)
   server.use(loggingMiddleware(app.db))
   server.use(checkDbMiddleware(app.db))
   return server
